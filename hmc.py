dcaa571d3811b36159b54b62ad1398b265451763
Halil Bilgin <bilginhalil@gmail.com>
2018-07-20 23:17:30 +0100

    Update the AIS to allow for taking gradients over integrals

--- hmc.py
+++ hmc.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 def kinetic_energy(v):
-    return 0.5 * tf.reduce_sum(tf.mul(v, v), axis=1)
+    return 0.5 * tf.reduce_sum(tf.multiply(v, v), axis=1)
 
 
 def hamiltonian(p, v, f):
@@ -16,15 +16,16 @@ def metropolis_hastings_accept(energy_prev, energy_next):
 
 def simulate_dynamics(initial_pos, initial_vel, stepsize, n_steps, energy_fn):
     def leapfrog(pos, vel, step, i):
-        dE_dpos = tf.gradients(tf.reduce_sum(energy_fn(pos)), pos)[0]
+        dE_dpos = tf.cast(tf.gradients(tf.reduce_sum(energy_fn(pos)), pos)[0], tf.float32)
         new_vel = vel - step * dE_dpos
         new_pos = pos + step * new_vel
+
         return [new_pos, new_vel, step, tf.add(i, 1)]
 
     def condition(pos, vel, step, i):
         return tf.less(i, n_steps)
 
-    dE_dpos = tf.gradients(tf.reduce_sum(energy_fn(initial_pos)), initial_pos)[0]
+    dE_dpos = tf.cast(tf.gradients(tf.reduce_sum(energy_fn(initial_pos)), initial_pos)[0], tf.float32)
     vel_half_step = initial_vel - 0.5 * stepsize * dE_dpos
     pos_full_step = initial_pos + stepsize * vel_half_step
 
@@ -54,12 +55,9 @@ def hmc_move(initial_pos, energy_fn, stepsize, n_steps):
 def hmc_updates(initial_pos, stepsize, avg_acceptance_rate, final_pos, accept,
                 target_acceptance_rate, stepsize_inc, stepsize_dec,
                 stepsize_min, stepsize_max, avg_acceptance_slowness):
-    new_pos = tf.select(accept, final_pos, initial_pos)
-    new_stepsize_ = tf.select(avg_acceptance_rate > target_acceptance_rate, stepsize_inc, stepsize_dec) * stepsize
+    new_pos = tf.where(accept, final_pos, initial_pos)
+    new_stepsize_ = tf.where(avg_acceptance_rate > target_acceptance_rate, stepsize_inc, stepsize_dec) * stepsize
     new_stepsize = tf.maximum(tf.minimum(new_stepsize_, stepsize_max), stepsize_min)
     new_acceptance_rate = tf.add(avg_acceptance_slowness * avg_acceptance_rate,
                                  (1.0 - avg_acceptance_slowness) * tf.reduce_mean(tf.to_float(accept)))
-    return new_pos, [
-        stepsize.assign(new_stepsize),
-        avg_acceptance_rate.assign(new_acceptance_rate)
-    ]
+    return new_pos, new_stepsize, new_acceptance_rate
--- test.py
+++ test.py
@@ -2,8 +2,6 @@ import tensorflow as tf
 import numpy as np
 import ais
 import matplotlib.pyplot as plt
-from priors import NormalPrior
-from kernels import ParsenDensityEstimator
 from scipy.stats import norm
 
 
@@ -15,22 +13,59 @@ class Generator(object):
     def __call__(self, z):
         return z * 2 + 3
 
+def kde_logpdf(x, mu, sigma):
+    """
+    Calculate the kde logpdf.
+    :param x: Shape [num_samples*batch_size, output_dim]
+    :param mu: Shape [num_samples*batch_size, output_dim]
+    :param sigma: variance
+    :return: [num_samples*batch_size]
+    """
+    # K(u) = 1/sqrt(2*pi) * exp(-0.5 * u^2)
+    # p(x) = 1/nh * |sum over n, i|{K((x-xi)/h)}
+    # logp(x) = -log(nh) + |sum over n, i|{-log(sqrt(2*pi) + 0.5*((mu-xi)/h)^2}
+    # logp(x)[i] = -log(h) -n*log(sqrt(2*pi)) -log(sqrt(2*pi) + 0.5*((mu-xi)/h)^2
+    # instead of summing it then taking the average of it, we simply keep things separately in a
+    # matrix. log_mean_exp will run in the end of AIS method. and we will obtain a shape
+    # [batch_size] instead of [batch_size*num_samples]
+
+    d = (x - mu) / sigma
+    e = -0.5 * tf.multiply(d, d)
+
+    z = tf.cast(tf.to_float(tf.shape(mu)[1]) *
+                tf.log(np.float32(sigma * np.sqrt(np.pi * 2.0))), tf.float32)
+
+    return e - z
 
 generator = Generator(1, 1)
-prior = NormalPrior()
-kernel = ParsenDensityEstimator()
-model = ais.Model(generator, prior, kernel, 0.25, 10000)
 
 p = norm()
-x = np.linspace(norm.ppf(0.01, loc=3, scale=2), norm.ppf(0.99, loc=3, scale=2), 100)
+
+batch_size = 40
+num_samples = 10000
+x = np.linspace(norm.ppf(0.01, loc=3, scale=2), norm.ppf(0.99, loc=3, scale=2), batch_size)
 p1 = norm.pdf(x, loc=3, scale=2)
-xx = np.reshape(x, [100, 1])
 
-schedule = ais.get_schedule(100, rad=4)
-print(schedule)
-p2 = np.exp(model.ais(xx, schedule))
+x_ph = tf.placeholder(tf.float32, [None, 1], name='x')
+model = ais.AIS(x_ph, lambda x, z: kde_logpdf(x, generator(z), 1.5),
+                  {'input_dim': 1, 'output_dim': 1, 'batch_size': batch_size},
+                  num_samples)
 
+xx = np.reshape(x, [batch_size, 1])
 
+schedule = ais.get_schedule(5, rad=4)
+
+#print(schedule)
+target = tf.tile(tf.expand_dims(model.x, 0), [model.num_samples, 1,1])
+model.x = tf.reshape(target, [model.num_samples * model.batch_size, model.dims['output_dim']])
+
+lld = model.ais(tf.distributions.Normal(loc=[0.], scale=[1.]).sample(num_samples*batch_size),
+                schedule)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+p2 = sess.run([lld], {x_ph: xx})
 plt.plot(x, p1)
-plt.plot(x, p2)
+plt.plot(x, np.exp(p2[0]))
 plt.show()
+
